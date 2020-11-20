deed61c92f6e0e3213cd129f24e99fab0ae0b8c7
Allan Galdino <allan.g.galdino@outlook.com>
2020-11-19 21:40:19 -0300

    top landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,22 @@
-import { LandingContainer } from '../styles/pages/Landing'
+import { FiHelpCircle } from 'react-icons/fi'
+
+import {
+  LandingContainer,
+  Title,
+  Info,
+  LandingHeader,
+  Content
+} from '../styles/pages/Landing'
+
 const Landing = () => (
   <LandingContainer>
-    <h1>Landing</h1>
+    <LandingHeader>
+      <Title>Objetos encontrados</Title>
+      <Info>
+        <FiHelpCircle />
+      </Info>
+    </LandingHeader>
+    <Content />
   </LandingContainer>
 )
 
--- src/styles/pages/Landing.js
+++ src/styles/pages/Landing.js
@@ -5,6 +5,8 @@ export const LandingContainer = styled.div`
   max-width: 1275px;
   min-height: 1144px;
   background-color: #FCFCFC;
+  display: flex;
+  flex-direction: column;
   
   @media screen and (min-width: 1400px) {
     margin: 80px auto;
@@ -12,4 +14,33 @@ export const LandingContainer = styled.div`
 
   padding: 20px;
   padding-bottom: 50px;
+`
+
+export const Title = styled.h1`
+  font-size: 30px;
+`
+
+export const LandingHeader = styled.div`
+  margin: 36px 56px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+export const Info = styled.div`
+  width: 100px;
+  height: 100px;
+  box-shadow: 2px 4px 13px rgba(0, 0, 0, 0.39);
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  svg{
+    width: 70%;
+    height: 70%;
+    color: #5c5c5c;
+  }
+`
+export const Content = styled.div`
+  
 `
