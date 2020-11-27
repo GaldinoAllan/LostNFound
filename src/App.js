11915cf0d42fb85a85ceaabf5982421dc8f91997
Allan Galdino <allan.g.galdino@outlook.com>
2020-11-27 15:10:13 -0300

    added all routes needed for the application

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Footer from './components/Footer'
 
 import Landing from './pages/Landing'
 import FAQ from './pages/FAQ'
+// import Login from './pages/Login'
+// import Register from './pages/Register'
+// import Item from './pages/Item'
 
 import GlobalStyle from './styles/globalStyles'
 
@@ -16,6 +19,10 @@ function App() {
       <Switch>
         <Route path='/' exact component={Landing} />
         <Route path='/faq' component={FAQ} />
+        {/* <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+        <Route path='/item' component={Item} />
+        <Route path='/item/:id' component={Item} /> */}
       </Switch>
       <Footer />
     </>
