eb4f8b1b3ca516b4034c1d404bcb4698b16b8d5a
Allan Galdino <allan.g.galdino@outlook.com>
2020-11-30 10:30:08 -0300

    deleted older files

--- Desenvolvimento/3.Implementacao/frontend/src/service/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: "http://localhost/lostnfound/Desenvolvimento/3.Implementacao/backend/controller/php/"
-});
-
-export default api;
